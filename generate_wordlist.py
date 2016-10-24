32da47e216067cfa7cf0a39813b0e0e361d5c5b3
Ben Woodfield <raser.13@hotmail.com>
2016-10-24 22:45:32 +0100

    Update generate_wordlist.py

--- generate_wordlist.py
+++ generate_wordlist.py
@@ -3,7 +3,7 @@
 
 # Creates ALL possible instances (combinations) of 'abc', including
 # the same letters more than once
-
+ 
 import itertools
 
 res = itertools.product('abc', repeat=5)# 5 characters long results
